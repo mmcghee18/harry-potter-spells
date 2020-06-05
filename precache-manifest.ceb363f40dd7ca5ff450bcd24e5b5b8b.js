88f18750838dbace1d4a6e226e275d08d1885381
Michelle <michmac202@gmail.com>
2020-06-04 17:06:37 -0700

    Updates

---
+++ precache-manifest.ceb363f40dd7ca5ff450bcd24e5b5b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a63f0e631de3621cd69502a516e49b6",
+    "url": "/scrollytelling-charts/index.html"
+  },
+  {
+    "revision": "7b4776b4c48b70cd7df6",
+    "url": "/scrollytelling-charts/static/js/2.d10cd24f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/scrollytelling-charts/static/js/2.d10cd24f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8293a2b1639d494685fa",
+    "url": "/scrollytelling-charts/static/js/main.2279779e.chunk.js"
+  },
+  {
+    "revision": "3703d9af5ee8e977c104",
+    "url": "/scrollytelling-charts/static/js/runtime-main.9855ebac.js"
+  }
+]);
