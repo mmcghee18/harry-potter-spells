c0e172ff911f2d08aec9e8754b4c731a8531a435
Michelle <michmac202@gmail.com>
2020-06-18 16:40:01 -0700

    hoverable tooltips

--- src/data-viz/CustomRadialChart.jsx
+++ src/data-viz/CustomRadialChart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { arc } from "d3-shape";
 import { scaleLinear } from "d3-scale";
@@ -13,11 +13,28 @@ const Pieces = styled.g`
   transform: translate(${canvasWidth / 2}px, ${canvasHeight / 2}px);
 `;
 
+const Tooltip = styled.div`
+  position: absolute;
+  top: ${(props) => props.$y}px;
+  left: ${(props) => props.$x}px;
+  display: flex;
+  background-color: white;
+  color: black;
+  border-radius: 10px;
+  height: 80px;
+  width: 180px;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+`;
+
 const SVGCanvas = styled.svg`
   margin: ${margin}px;
   position: sticky;
   top: ${margin}px;
 `;
+
+const AnnotationLayer = styled.div``;
 
 const mostMentions = (data) => {
   return _.get(
@@ -26,41 +43,76 @@ const mostMentions = (data) => {
   );
 };
 
-const Piece = ({ data, i, scale, numBars }) => {
+const Piece = ({
+  data,
+  i,
+  scale,
+  numBars,
+  setHoveredPiece,
+  setMouseX,
+  setMouseY,
+}) => {
   const arcGenerator = arc()
     .startAngle(((Math.PI * 2) / numBars) * i)
     .endAngle(((Math.PI * 2) / numBars) * (i + 1))
     .innerRadius(0)
     .outerRadius(scale(data.mentions));
 
-  console.log(`My outer radius is ${scale(data.mentions)}`);
-
   return (
-    <g className={data.type.toLowerCase()}>
+    <g
+      className={data.type.toLowerCase()}
+      onMouseEnter={(e) => {
+        setHoveredPiece(data);
+        setMouseX(e.nativeEvent.pageX);
+        setMouseY(e.nativeEvent.pageY);
+      }}
+      onMouseLeave={() => {
+        setHoveredPiece(null);
+        setMouseX(null);
+        setMouseY(null);
+      }}
+    >
       <path d={arcGenerator()} />
     </g>
   );
 };
 
 const CustomRadialChart = ({ data }) => {
+  const [hoveredPiece, setHoveredPiece] = useState(null);
+  const [mouseX, setMouseX] = useState(null);
+  const [mouseY, setMouseY] = useState(null);
+  console.log(mouseX, mouseY);
+
   const scale = scaleLinear()
     .domain([0, mostMentions(data)])
     .range([0, canvasWidth / 2]);
 
   return (
-    <SVGCanvas height={canvasHeight} width={canvasWidth}>
-      <Pieces>
-        {_.map(data, (d, i) => (
-          <Piece
-            key={d.spell}
-            data={d}
-            i={i}
-            scale={scale}
-            numBars={data.length}
-          />
-        ))}
-      </Pieces>
-    </SVGCanvas>
+    <>
+      <SVGCanvas height={canvasHeight} width={canvasWidth}>
+        <Pieces>
+          {_.map(data, (d, i) => (
+            <Piece
+              key={d.spell}
+              data={d}
+              i={i}
+              scale={scale}
+              numBars={data.length}
+              setHoveredPiece={setHoveredPiece}
+              setMouseX={setMouseX}
+              setMouseY={setMouseY}
+            />
+          ))}
+        </Pieces>
+      </SVGCanvas>
+      <AnnotationLayer>
+        {hoveredPiece && (
+          <Tooltip key={hoveredPiece.spell} $x={mouseX} $y={mouseY}>
+            {hoveredPiece.spell} : {hoveredPiece.mentions}
+          </Tooltip>
+        )}
+      </AnnotationLayer>
+    </>
   );
 };
 
