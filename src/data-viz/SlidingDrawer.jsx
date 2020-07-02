aa8d6fcb7a0e31b51398f4741ce1eebf1e5cad6d
Michelle <michmac202@gmail.com>
2020-07-02 15:57:40 -0500

    adding more words to spellMentions, cleaning up spellCounts

--- src/data-viz/SlidingDrawer.jsx
+++ src/data-viz/SlidingDrawer.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import _ from "lodash";
 import { Drawer } from "antd";
-import { animated, config, useSprings } from "react-spring";
-import { scaleLinear } from "d3-scale";
-import bookLengths from "../data/bookLengths.js";
 import bookTitles from "../data/bookTitles.js";
-import useChartDimensions from "../hooks/useChartDimensions.js";
+import Timeline from "./Timeline.jsx";
 
 const Mention = styled.p`
   color: black;
@@ -26,49 +23,6 @@ const BookTitle = styled.h1`
 const ItalicizedEffect = styled.p`
   font-style: italic;
 `;
-
-const Timeline = ({ book, mentions, clickedSpell }) => {
-  const chartSettings = { height: 50 };
-  const [ref, dms] = useChartDimensions(chartSettings);
-  const domain = [0, bookLengths[book]]; // book character indicies
-  const range = [10, dms.width - 20]; // width of line
-  const xScale = scaleLinear().domain(domain).range(range);
-  const circleR = 3;
-
-  const [springs, set, stop] = useSprings(mentions.length, (m) => ({
-    cx: -20,
-  }));
-  set((m) => {
-    return clickedSpell
-      ? {
-          cx: xScale(mentions[m].index),
-          delay: 500,
-          config: { mass: 1, tension: 160, friction: 17 },
-        }
-      : { cx: -20 };
-  });
-
-  return (
-    <div ref={ref}>
-      <svg width={dms.width} height={dms.height}>
-        <path
-          d={["M", range[0], chartSettings.height / 2, "H", range[1]].join(" ")}
-          stroke="grey"
-        />
-        {springs.map((props, i) => (
-          <>
-            <animated.circle
-              key={i}
-              cx={props.cx}
-              cy={chartSettings.height / 2}
-              r={circleR}
-            />
-          </>
-        ))}
-      </svg>
-    </div>
-  );
-};
 
 const SlidingDrawer = ({ clickedSpell, setClickedSpell, mentions, book }) => {
   const drawerHeader = (
---
+++ src/data-viz/Timeline.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import _ from "lodash";
+import { animated, useSprings } from "react-spring";
+import { scaleLinear } from "d3-scale";
+import bookLengths from "../data/bookLengths.js";
+import useChartDimensions from "../hooks/useChartDimensions.js";
+
+const Timeline = ({ book, mentions, clickedSpell }) => {
+  const chartSettings = { height: 50 };
+  const [ref, dms] = useChartDimensions(chartSettings);
+  const domain = [0, bookLengths[book]]; // book character indicies
+  const range = [10, dms.width - 20]; // width of line
+  const xScale = scaleLinear().domain(domain).range(range);
+  const circleR = 3;
+
+  const [springs, set] = useSprings(mentions.length, (m) => ({
+    cx: -20,
+  }));
+  set((m) => {
+    return clickedSpell
+      ? {
+          cx: xScale(mentions[m].index),
+          delay: 500,
+          config: { mass: 1, tension: 160, friction: 17 },
+        }
+      : { cx: -20 };
+  });
+
+  return (
+    <div ref={ref}>
+      <svg width={dms.width} height={dms.height}>
+        <path
+          d={["M", range[0], chartSettings.height / 2, "H", range[1]].join(" ")}
+          stroke="grey"
+        />
+        {springs.map((props, i) => (
+          <>
+            <animated.circle
+              key={i}
+              cx={props.cx}
+              cy={chartSettings.height / 2}
+              r={circleR}
+            />
+          </>
+        ))}
+      </svg>
+    </div>
+  );
+};
+
+export default Timeline;
